56773ed18ab5ebc77d46b32acf4442379d2423e9
DiegoQR <diegoqr2000@gmail.com>
2026-03-10 21:11:35 -0400

    Test workflows

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
+  // Cambio para probar los workflows de GitHub Actions
   apiUrl: 'https://maneja-tus-gastos-api-5k3d.onrender.com',
 };
 
